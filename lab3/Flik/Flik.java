c94e49f62706f80bc5d677d86ca4c3a7c8e4f63a
h-shawn <hwangxiao@sjtu.edu.cn>
2024-03-09 22:29:22 +0800

    completed lab3

---
+++ lab3/Flik/Flik.java
@@ -0,0 +1,8 @@
+/** An Integer tester created by Flik Enterprises. */
+public class Flik {
+    // For ==, when num < 128 compare num, num >= 128 compare address
+    public static boolean isSameNumber(Integer a, Integer b) {
+        return a.equals(b);
+    }
+
+}
---
+++ lab3/Flik/FlikTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FlikTest {
+    @Test
+    public void TestFlik(){
+        assertTrue(Flik.isSameNumber(128, 128));
+    }
+}
